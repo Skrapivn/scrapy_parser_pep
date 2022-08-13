ed9c2c97a745fe729b22706479a88fbfda18b112
Sergey Krapivnitskiy <ya.uplink@yandex.ru>
2022-08-13 15:44:40 +0300

    work items

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class PepSpider(scrapy.Spider):
@@ -7,4 +8,21 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        """Собирает ссылки на документы PEP"""
+        index = response.xpath('//*[@id="numerical-index"]').css("tbody")
+        all_hrefs = index.css("a::attr(href)").getall()
+        for href in all_hrefs:
+            yield response.follow(href, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        """Парсит страницы с документами и формирует Items"""
+        pep = response.css("section[id='pep-content']")
+        pattern = re.compile(r"^PEP\s(?P<number>\d+)[\s–]+(?P<name>.*)")
+        h1_tag = pattern.search(pep.css("h1::text").get())
+        if h1_tag:
+            number, name = h1_tag.group("number", "name")
+        yield {
+            'number': number,
+            'name': name,
+            'status': pep.css("dt:contains('Status') + dd::text").get()
+        }
